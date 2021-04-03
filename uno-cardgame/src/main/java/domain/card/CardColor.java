93b46ad8cb01f9417ff531d6ea572d29256998f7
martinnguyen42 <ndphu.works@gmail.com>
2021-04-03 17:10:22 +0700

    Create Card classes

---
+++ uno-cardgame/src/main/java/domain/card/CardColor.java
@@ -0,0 +1,9 @@
+package domain.card;
+
+// Card color using in game
+public enum CardColor {
+	RED,
+	GREEN,
+	BLUE,
+	YELLOW
+}
---
+++ uno-cardgame/src/main/java/domain/card/NumberCard.java
@@ -0,0 +1,27 @@
+package domain.card;
+
+public class NumberCard implements Card {
+
+	private final int value;
+	private final CardColor color;
+	
+	public NumberCard(int value, CardColor color) {
+		super();
+		this.value = value;
+		this.color = color;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public CardColor getColor() {
+		return color;
+	}
+
+	@Override
+	public CardType getType() {
+		return CardType.NUMBER;
+	}
+	
+}
